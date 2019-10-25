84559cdea72d8c632f12514356564f9e7998c2e7
Aishwarya Lakshminarasimhan <aishwaryarln@gmail.com>
2019-10-25 04:05:03 +0000

    Added frontend UI

--- rProject/script.R
+++ rProject/script.R
@@ -6,9 +6,38 @@ handler <- function(...) {
     # multiValueQueryStringParameters,pathParameters,stageVariables,requestContext,body,isBase64Encoded]
     
     # print(requestInfo)
-    print("Hi from lambda")
-    return(list(
-        statusCode = 200,
-        body = "Hi from the R Lambda Function :)"
-    ))
+    
+    #print(requestInfo['httpMethod'])
+    if(requestInfo['httpMethod'] == "GET") {
+        fileName <- 'index.html'
+        indexContents <- readChar(fileName, file.info(fileName)$size)
+        return (list(
+            statusCode = 200,
+            headers = list("Content-Type" = "text/html"),
+            body = indexContents
+        ))
+    }
+    else if(requestInfo['httpMethod'] == "POST") {
+        request = requestInfo["body"]
+        responseContent <- toJSON(list(
+                isComplete = TRUE,
+                htmlFeedback = paste('<h2 style="color:green">User\'s response : </h2>', request, sep="<br/>"),
+                textFeedback = paste("User's response : \n", request, sep=" "),
+                jsonFeedback = request
+            ), auto_unbox = TRUE)
+        result <- list(
+            statusCode = 200,
+            headers = list("Content-Type" = "application/json"),
+            body = responseContent
+        )
+        #print(result)
+        return (result)
+    }
+    else {
+        return (list(
+            statusCode = 200,
+            body = "Hi from the R Lambda Function :)"
+        ))
+    }
+    
 }
